1709d8ce8b5cd39a35bb004760fb855e1fa6079d
Jordan Davis <jdavisdev@gmail.com>
2024-12-28 20:54:38 -0700

    add share

--- src/App.tsx
+++ src/App.tsx
@@ -128,8 +128,32 @@ export default function App() {
       <WordHistory wordHistory={submittedWords} />
       <Timer />
       <Score score={score} threeLetterWordCount={threeLetterWordCount} />
+      <Share />
     </>
   )
+}
+
+function Share() {  
+  const handleShare = async () => {
+    if (navigator.share) {
+      try {
+        await navigator.share({
+          title: "Quackle",
+          text: "You suck!",
+          url: "https://jdavisdev.github.io/perquacky-main/",
+        });
+      } catch (error) {
+        console.error('Error sharing:', error);
+      }
+    } else {
+      // Fallback behavior if Web Share API is not supported
+      console.log('Web Share API not supported');
+    }
+  };
+
+  return (
+    <button onClick={handleShare}>Share</button>
+  );
 }
 
 function LettersGrid(props) {
