09d639964fb6bd0f087d65fee15eb7fcc69151eb
Jordan Davis <jdavisdev@gmail.com>
2025-01-25 14:47:41 -0700

    add letter

--- src/LettersGrid.tsx
+++ src/LettersGrid.tsx
@@ -47,19 +47,147 @@ export default function LettersGrid({
     );
   }
 
+  // return (
+  //   <>
+  //     <div className="grid-container">
+  //       {letters.map((item, index) => (
+  //         <button
+  //           key={index}
+  //           className="grid-cell"
+  //           disabled={isTileDisabled(item, hasStarted)}
+  //           onClick={() => handleLetterClick(item)}
+  //         >
+  //           {hasStarted ? item : "?"}
+  //         </button>
+  //       ))}
+  //     </div>
+  //     <br></br>
+  //     <span className="button-row">
+  //       <button className="clear" onClick={clearWord}>
+  //         X
+  //       </button>
+  //       <button className="shuffle-button" onClick={handleShuffleClick}>
+  //         <FontAwesomeIcon icon={faRandom} className="shuffle-icon" />
+  //       </button>
+  //       <button className="submit" onClick={submitWord}>
+  //         Submit
+  //       </button>
+  //     </span>
+  //   </>
+  // );
+
   return (
     <>
       <div className="grid-container">
-        {letters.map((item, index) => (
-          <button
-            key={index}
-            className="grid-cell"
-            disabled={isTileDisabled(item, hasStarted)}
-            onClick={() => handleLetterClick(item)}
-          >
-            {hasStarted ? item : "?"}
-          </button>
-        ))}
+        {/* first column */}
+        <div className="column">
+          <div className="grid-cell">
+            <button
+              key={letters[0]}
+              className="grid-cell"
+              disabled={isTileDisabled(letters[0], hasStarted)}
+              onClick={() => handleLetterClick(letters[0])}
+            >
+              {hasStarted ? letters[0] : "?"}
+            </button>
+          </div>
+          <div className="grid-cell">
+            <button
+              key={letters[1]}
+              className="grid-cell"
+              disabled={isTileDisabled(letters[1], hasStarted)}
+              onClick={() => handleLetterClick(letters[1])}
+            >
+              {hasStarted ? letters[1] : "?"}
+            </button>
+          </div>
+          <div className="grid-cell">
+            <button
+              key={letters[2]}
+              className="grid-cell"
+              disabled={isTileDisabled(letters[2], hasStarted)}
+              onClick={() => handleLetterClick(letters[2])}
+            >
+              {hasStarted ? letters[2] : "?"}
+            </button>
+          </div>
+        </div>
+
+        <div className="column">
+          <div className="grid-cell">
+            <button
+              key={letters[3]}
+              className="grid-cell"
+              disabled={isTileDisabled(letters[3], hasStarted)}
+              onClick={() => handleLetterClick(letters[3])}
+            >
+              {hasStarted ? letters[3] : "?"}
+            </button>
+          </div>
+          <div className="grid-cell">
+            <button
+              key={letters[4]}
+              className="grid-cell"
+              disabled={isTileDisabled(letters[4], hasStarted)}
+              onClick={() => handleLetterClick(letters[4])}
+            >
+              {hasStarted ? letters[4] : "?"}
+            </button>
+          </div>
+          <div className="grid-cell">
+            <button
+              key={letters[5]}
+              className="grid-cell"
+              disabled={isTileDisabled(letters[5], hasStarted)}
+              onClick={() => handleLetterClick(letters[5])}
+            >
+              {hasStarted ? letters[5] : "?"}
+            </button>
+          </div>
+          <div className="grid-cell">
+            <button
+              key={letters[6]}
+              className="grid-cell"
+              disabled={isTileDisabled(letters[6], hasStarted)}
+              onClick={() => handleLetterClick(letters[6])}
+            >
+              {hasStarted ? letters[6] : "?"}
+            </button>
+          </div>
+        </div>
+
+        <div className="column">
+          <div className="grid-cell">
+            <button
+              key={letters[7]}
+              className="grid-cell"
+              disabled={isTileDisabled(letters[7], hasStarted)}
+              onClick={() => handleLetterClick(letters[7])}
+            >
+              {hasStarted ? letters[7] : "?"}
+            </button>
+          </div>
+          <div className="grid-cell">
+            <button
+              key={letters[8]}
+              className="grid-cell"
+              disabled={isTileDisabled(letters[8], hasStarted)}
+              onClick={() => handleLetterClick(letters[8])}
+            >
+              {hasStarted ? letters[8] : "?"}
+            </button>
+          </div>
+          <div className="grid-cell">
+            <button
+              key={letters[9]}
+              className="grid-cell"
+              disabled={isTileDisabled(letters[9], hasStarted)}
+              onClick={() => handleLetterClick(letters[9])}
+            >
+              {hasStarted ? letters[9] : "?"}
+            </button>
+          </div>
+        </div>
       </div>
       <br></br>
       <span className="button-row">
